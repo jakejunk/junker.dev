eeb103b93da03f82bc0b2f3bc43c9c7a3007d70f
Jake <jake@jakejunker.com>
2022-07-15 21:40:40 -0600

    Add CircleCI deployment step + small change to verify

--- src/main/kotlin/dev/junker/index/Index.kt
+++ src/main/kotlin/dev/junker/index/Index.kt
@@ -5,6 +5,7 @@ import kotlinx.html.*
 fun HTML.renderIndex() {
     head {
         link(rel = "stylesheet", href = "/styles.css", type = "text/css")
+        title("Jake Junker")
     }
     body {
         h1(classes = "page-title") {
