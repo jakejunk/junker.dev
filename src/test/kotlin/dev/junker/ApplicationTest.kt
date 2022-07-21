2487426311c923e8cedbf287310b1d035decb46e
Jake <jake@jakejunker.com>
2022-07-20 23:15:13 -0600

    Fix and update tests

--- src/test/kotlin/dev/junker/ApplicationTest.kt
+++ src/test/kotlin/dev/junker/ApplicationTest.kt
@@ -1,5 +1,6 @@
 package dev.junker
 
+import dev.junker.about.renderAbout
 import dev.junker.index.renderIndex
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -14,12 +15,26 @@ import kotlin.test.assertEquals
 class ApplicationTest {
     @Test
     fun testIndex() = testApplication {
-        application {
-            webMain()
-        }
-
         val expected = generateDocumentAsText { renderIndex() }
         val response = client.get("/")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(expected, response.bodyAsText())
+    }
+
+    @Test
+    fun testAbout() = testApplication {
+        val expected = generateDocumentAsText { renderAbout() }
+        val response = client.get("/about")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(expected, response.bodyAsText())
+    }
+
+    @Test
+    fun testAboutWithTrailingSlash() = testApplication {
+        val expected = generateDocumentAsText { renderAbout() }
+        val response = client.get("/about/")
 
         assertEquals(HttpStatusCode.OK, response.status)
         assertEquals(expected, response.bodyAsText())
