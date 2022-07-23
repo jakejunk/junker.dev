7ea96da8037bc7371c15df9edcffbe8ebbab84f3
Jake <jake@jakejunker.com>
2022-07-22 18:33:31 -0600

    Add tests for 404s and 500s

--- src/test/kotlin/dev/junker/ApplicationTest.kt
+++ src/test/kotlin/dev/junker/ApplicationTest.kt
@@ -3,8 +3,8 @@ package dev.junker
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.routing.*
 import io.ktor.server.testing.*
-import kotlinx.html.FlowContent
 import kotlinx.html.html
 import kotlinx.html.stream.createHTML
 import kotlin.test.Test
@@ -35,6 +35,30 @@ class ApplicationTest {
         val response = client.get("/about/")
 
         assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals(expected, response.bodyAsText())
+    }
+
+    @Test
+    fun testNotFound() = testApplication {
+        val expected = generateDocumentAsText(Page.NotFound)
+        val response = client.get("/totalAbsoluteNonsense/")
+
+        assertEquals(HttpStatusCode.NotFound, response.status)
+        assertEquals(expected, response.bodyAsText())
+    }
+
+    @Test
+    fun testInternalServerError() = testApplication {
+        routing {
+            get("/broken") {
+                throw RuntimeException()
+            }
+        }
+
+        val expected = generateDocumentAsText(Page.InternalServerError)
+        val response = client.get("/broken")
+
+        assertEquals(HttpStatusCode.InternalServerError, response.status)
         assertEquals(expected, response.bodyAsText())
     }
 
